77ddc5e9e2dd1603f77ee318ddc0c3018404988a
Pedro Foresti Leão <forestileao@pop-os.localdomain>
2023-04-13 18:10:29 -0300

    fixed testbenches

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ ghdl -a mux4x1.vhd
 ghdl -a ula.vhd
 ghdl -a reg.vhd
 ghdl -a reg_bank.vhd
+ghdl -a processor.vhd
 ghdl -a reg_bank_tb.vhd
 
 ghdl -r reg_bank_tb --wave=compiled/reg_bank_tb.ghw
--- processor_tb.vhd
+++ processor_tb.vhd
@@ -2,41 +2,38 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity reg_bank_tb is
+entity processor_tb is
 end entity;
 
-architecture a_reg_bank_tb of reg_bank_tb is
-    component reg_bank
-        port(
-            clk:       in std_logic;
-            rst:       in std_logic;
-            wr_en:     in std_logic;
-            address_read_0:   in unsigned(2 downto 0);
-            address_read_1:   in unsigned(2 downto 0);
-            address_write:   in unsigned(2 downto 0);
-            data_in:   in unsigned(15 downto 0);
-            data_out_0:  out unsigned(15 downto 0);
-            data_out_1:  out unsigned(15 downto 0)
+architecture a_processor of processor_tb is
+    component processor is
+        port (
+            rst, clk, wr_en: in std_logic;
+            address_read_0, address_read_1, address_write: in unsigned(2 downto 0);
+            op: in unsigned(1 downto 0);
+            data_in: in unsigned(15 downto 0);
+            data_out: out unsigned(15 downto 0)
         );
     end component;
 
     signal rst, clk, wr_en, finished: std_logic;
     signal address_read_0, address_read_1, address_write: unsigned(2 downto 0);
+    signal op: unsigned(1 downto 0);
     signal data_in: unsigned(15 downto 0);
-    signal data_out_0, data_out_1: unsigned(15 downto 0);
+    signal data_out: unsigned(15 downto 0);
 
     constant period_time : time := 25 ns;
 begin
-    uut: reg_bank port map(
-        clk => clk,
+    uut: processor port map(
         rst => rst,
+        clk => clk,
         wr_en => wr_en,
         address_read_0 => address_read_0,
         address_read_1 => address_read_1,
         address_write => address_write,
+        op => op,
         data_in => data_in,
-        data_out_0 => data_out_0,
-        data_out_1 => data_out_1
+        data_out => data_out
     );
 
     rst_global: process
@@ -69,9 +66,6 @@ begin
 
     process
     begin
-        -- output must must be zero
-        address_read_0 <= "000";
-        address_read_1 <= "000";
         wait for 200 ns;
         wr_en <= '1';
         address_read_0 <= "001";
@@ -83,12 +77,10 @@ begin
         wait for 100 ns;
         address_write <= "010"; -- write on reg_2
         data_in <= "1111111100000000";
-        
-        wait for 100 ns;
-        address_read_1 <= "111";
-        address_write <= "111"; -- write on reg_7
-        data_in <= "1010101010101010";
+        op <= "00"; -- sum
+        wait for 50 ns;
+        wr_en <= '0';
         wait;
     end process;
 
-end architecture a_reg_bank_tb;
+end architecture;
