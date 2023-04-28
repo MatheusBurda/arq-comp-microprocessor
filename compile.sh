d288eb9f6472fed3ddf724efb439b81576c34e48
MatheusBurda <matheusburda@alunos.utfpr.edu.br>
2023-04-28 11:05:46 -0300

    Organized all testbenches on test folder

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Entities
 ghdl -a mux4x1.vhd
 ghdl -a mux2x1.vhd
 ghdl -a ula.vhd
@@ -10,7 +11,10 @@ ghdl -a rom.vhd
 ghdl -a processor.vhd
 ghdl -a state_machine.vhd
 ghdl -a control_unit.vhd
-ghdl -a control_unit_tb.vhd
 
+# Testbanch
+ghdl -a test/control_unit_tb.vhd
+
+# Run and view waveform on gtkwave
 ghdl -r control_unit_tb --wave=compiled/control_unit_tb.ghw
 gtkwave compiled/control_unit_tb.ghw 
