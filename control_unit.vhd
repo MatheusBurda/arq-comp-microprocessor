6fd744084bd988db8ae58123987dccf5a46bf239
forestileao <pedroforestileao@gmail.com>
2023-05-10 09:37:27 -0300

    refactored control unit and processor

--- control_unit.vhd
+++ control_unit.vhd
@@ -6,31 +6,14 @@ entity control_unit is
     port(
         clk:       in std_logic;
         rst:       in std_logic;
-        rom_out: out unsigned(13 downto 0);
+        rom: in unsigned(13 downto 0);
         pc_out: out unsigned(6 downto 0);
-        alu_src, reg_write: out std_logic
+        alu_src, reg_write, pc_wr_en, jump_en: out std_logic;
+        alu_op: out unsigned(1 downto 0)
     );
 end entity;
 
 architecture a_control_unit of control_unit is
-    component pc
-        port(
-            clk:       in std_logic;
-            rst:       in std_logic;
-            wr_en:     in std_logic;
-            data_in:   in unsigned(6 downto 0);
-            data_out:  out unsigned(6 downto 0)
-        );
-    end component;
-
-    component rom is
-        port(
-            clk     : in std_logic;
-            address : in unsigned(6 downto 0);
-            data    : out unsigned(13 downto 0)
-        );
-    end component;
-
     component state_machine is
         port(
             clk:    in std_logic;
@@ -41,42 +24,23 @@ architecture a_control_unit of control_unit is
 
     signal pc_out_sig, pc_data_in, jump_address: unsigned(6 downto 0);
     signal opcode: unsigned(3 downto 0);
-    signal rom_out_sig: unsigned(13 downto 0);
-    signal pc_wr_en, jump_en, load_constant: std_logic;
-    signal alu_op, state_sig: unsigned(1 downto 0);
+    signal state_sig: unsigned(1 downto 0);
 
 begin
+
     state_machine_pm: state_machine port map(
         clk => clk,
         rst => rst,
         state => state_sig
     );
 
-    rom_pm: rom port map(
-        clk => clk,
-        address => pc_out_sig,
-        data => rom_out_sig
-    );
-    rom_out <= rom_out_sig;
-
-    pc_pm: pc port map(
-        clk => clk,
-        rst => rst,
-        wr_en => pc_wr_en,
-        data_in => pc_data_in,
-        data_out => pc_out_sig
-    );
-
     -- Instruction Fetch
-        pc_data_in <= pc_out_sig + "0000001" when jump_en = '0' else jump_address;
         pc_wr_en <= '0' when state_sig = "00" else '1';
-        pc_out <= pc_out_sig;
-        opcode <= rom_out_sig(13 downto 10);
+        opcode <= rom(13 downto 10);
 
     -- Instruction Decode
         --* jump opcode "1111, jumps to the exact 7 bits rom address"
         jump_en <= '1' when opcode = "1111" else '0';
-        jump_address <= rom_out_sig(6 downto 0);
 
         --* LDI opcode 0001
         --* MOV opcode 0010
--- processor.vhd
+++ processor.vhd
@@ -43,8 +43,41 @@ architecture a_processor of processor is
             output          : out unsigned(15 downto 0)
         );
     end component;
+
+    component control_unit is
+        port(
+            clk:       in std_logic;
+            rst:       in std_logic;
+            rom: in unsigned(13 downto 0);
+            pc_out: out unsigned(6 downto 0);
+            alu_src, reg_write, pc_wr_en, jump_en: out std_logic;
+            alu_op: out unsigned(1 downto 0)
+        );
+    end component;
+
+    component rom is
+        port(
+            clk     : in std_logic;
+            address : in unsigned(6 downto 0);
+            data    : out unsigned(13 downto 0)
+        );
+    end component;
+
+    component pc
+        port(
+            clk:       in std_logic;
+            rst:       in std_logic;
+            wr_en:     in std_logic;
+            data_in:   in unsigned(6 downto 0);
+            data_out:  out unsigned(6 downto 0)
+        );
+    end component;
     
     signal reg_bank_out_0, ula_out, ula_src_mux_in, ula_src_mux_out: unsigned(15 downto 0);
+    signal rom_data: unsigned(13 downto 0);
+    signal pc_out_sig, pc_data_in, jump_address: unsigned(6 downto 0);
+    signal alu_src, reg_write, pc_wr_en, jump_en: std_logic;
+    signal alu_op: unsigned(1 downto 0);
 begin
     reg_bank_pm: reg_bank port map(
         clk => clk,
@@ -65,6 +98,24 @@ begin
         output => ula_out
     );
 
+    control_unit_pm: control_unit port map(
+        clk => clk,
+        rst => rst,
+        rom => rom_data,
+        pc_out => pc_out_sig,
+        alu_src => alu_src,
+        reg_write => reg_write,
+        pc_wr_en => pc_wr_en,
+        alu_op => alu_op,
+        jump_en => jump_en
+    );
+
+    rom_pm: rom port map(
+        clk => clk,
+        address => pc_out_sig,
+        data => rom_data
+    );
+
     ula_src_mux: mux2x1 port map(
         op => ula_src_mux_op,
         in0 => ula_src_mux_in,
@@ -72,5 +123,15 @@ begin
         output => ula_src_mux_out -- connected to 'ula' 'in1'
     );
 
+    pc_pm: pc port map(
+        clk => clk,
+        rst => rst,
+        wr_en => pc_wr_en,
+        data_in => pc_data_in,
+        data_out => pc_out_sig
+    );
+
+    jump_address <= rom_data(6 downto 0);
+    pc_data_in <= pc_out_sig + "0000001" when jump_en = '0' else jump_address;
     data_out <= ula_out;
 end architecture;
