8df49991d1dff503fc9bc629e3006e18cca2e20a
forestileao <pedroforestileao@gmail.com>
2023-06-09 21:14:06 -0300

    bugfix: fixed primes assembly

--- test/processor_tb.vhd
+++ test/processor_tb.vhd
@@ -31,7 +31,7 @@ begin
    
     sim_time_proc: process
         begin
-        wait for period_time * 1000;
+        wait for period_time * 4000;
         finished <= '1';
         wait;
     end process sim_time_proc;
